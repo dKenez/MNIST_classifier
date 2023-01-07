6dec7a4bed4c8399c2bc744ec1b7c2dd30f221a3
dKenez <47757462+dKenez@users.noreply.github.com>
2023-01-07 17:09:42 +0100

    Formatting and documentation

--- src/models/model.py
+++ src/models/model.py
@@ -1,6 +1,7 @@
 from torch import nn
 import torch.nn.functional as F
 
+
 class MyAwesomeModel(nn.Module):
     def __init__(self):
         super().__init__()
@@ -8,10 +9,10 @@ class MyAwesomeModel(nn.Module):
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, 64)
         self.fc4 = nn.Linear(64, 10)
-        
+
         # Dropout module with 0.2 drop probability
         self.dropout = nn.Dropout(p=0.2)
-        
+
     def forward(self, x):
         # make sure input tensor is flattened
         x = x.view(x.shape[0], -1)
@@ -20,8 +21,11 @@ class MyAwesomeModel(nn.Module):
         x = self.dropout(F.relu(self.fc1(x)))
         x = self.dropout(F.relu(self.fc2(x)))
         x = self.dropout(F.relu(self.fc3(x)))
-        
+
         # output so no dropout here
         x = F.log_softmax(self.fc4(x), dim=1)
-        
+  
         return x
+
+
+           
